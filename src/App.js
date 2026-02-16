142356b1ebb5b73a2ddbff0a89f3f5b54569947f
Wemerson Fernandes <wemersonf.rangel@outlook.com>
2026-02-16 18:55:17 -0300

    Create componente Login and fix build

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h2>Disney Plus Clone</h2>
+      <Router>
+        <Routes>
+          {/* Na v6/v7, usamos a prop 'element' e passamos o componente como JSX */}
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
