5b8ca298af5aac6e69c9509abe12f6ff89b1c9a7
Wemerson Fernandes <wemersonf.rangel@outlook.com>
2026-02-17 19:04:03 -0300

    Add logo images

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,9 @@ const Login = (props) => {
   return (
     <Container>
       <Content>
+        <CTA>
+          <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+        </CTA>
         <BgImage />
       </Content>
     </Container>
@@ -42,6 +45,21 @@ const BgImage = styled.div`
   right: 0;
   left: 0;
   z-index: -1;
+`;
+
+const CTA = styled.div`
+  width: 100%;
+  max-width: 650px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
 `;
 
 export default Login;
