e6ac3ad169d4be6269309f94fda38abe4912e5d8
Wemerson Fernandes <wemersonf.rangel@outlook.com>
2026-02-20 19:01:25 -0300

    Add second images logo

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,6 +11,7 @@ const Login = (props) => {
             with a Disney+ subscription. As of 03/26/21, the price of Disney+
             and The Disney Bundle will increase by $1.
           </Description>
+          <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
         </CTA>
         <BgImage />
       </Content>
@@ -92,5 +93,12 @@ const Description = styled.p`
   line-height: 1.5;
   letter-spacing: 1.5px;
 `;
+
+const CTALogoTwo = styled.img`
+max-width:600px ;
+margin-bottom:20px;
+display:inline-block;
+vertical-align:bottom;
+width:100%`
 
 export default Login;
