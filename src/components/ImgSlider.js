15d47abd6e9e0e94ea1dfc08b3a705fde752713d
Wemerson Fernandes <wemersonf.rangel@outlook.com>
2026-03-12 19:27:40 -0300

    Add React Slick Carousel

---
+++ src/components/ImgSlider.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const ImgSlider = (props) => {
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slideToShow: 1,
+    slideToScroll: 1,
+    autoplay: true,
+  };
+  return <Slider {...settings}></Slider>;
+};
+
+export default ImgSlider;
